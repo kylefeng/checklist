800a87a0683e3da8328052ac2e027a9f2ef55101
Kyle Feng <kylefengg@gmail.com>
2013-08-21 17:59:01 +0800

    Configurable header

--- src/checklist/core.clj
+++ src/checklist/core.clj
@@ -73,8 +73,32 @@
                 (swap! rows conj [title k item])))))))
     @rows))
 
-(defn- gen-excel [file-name rows]
-  (let [rows (cons ["大类" "小类" "检查点"] rows)
+(defn- normalize-rows [rows]
+  (let [max-len (reduce #(let [lhs %1
+                               rhs (count %2)]
+                           (if (> lhs rhs) lhs rhs))
+                        0
+                        rows)]
+    (map #(if (not= (count %) max-len)
+            (concat % (repeat (- max-len (count %)) ""))
+            %)
+         rows)))
+
+(defn- load-header [conf]
+  (-> (slurp conf)
+      str/trim
+      (str/split #" ")
+      vec))
+
+(defn- cal-merged-regions [excel-rows]
+  (let [cols (count (first excel-rows))
+        rows (count excel-rows)]
+
+    ))
+
+(defn- gen-excel [header-conf file-name rows]
+  (let [header (load-header header-conf)
+        rows (normalize-rows (cons header rows))
         sheet-name "checklist"
         wb (create-workbook sheet-name rows)
         sheet (select-sheet sheet-name wb)
@@ -82,11 +106,14 @@
         style (.createCellStyle wb)
         len-of-char (fn [n] (* n 256))]
     (do
+      ;; Set style of sheet
       (doto sheet
         (.setColumnWidth 0 (len-of-char 25))
         (.setColumnWidth 1 (len-of-char 35))
         (.setColumnWidth 2 (len-of-char 60))
-        (.createFreezePane 3 1))
+        (.setColumnWidth 3 (len-of-char 50))
+        (.createFreezePane (count header) 1))
+      ;; set stype of cells
       (doto style
         (.setWrapText true)
         (.setVerticalAlignment CellStyle/VERTICAL_CENTER)
@@ -99,22 +126,23 @@
                                                          :font {:bold true}}))
       (save-workbook! file-name wb))))
 
-(defn checklist-to-excel [dir file-name]
+(defn checklist-to-excel [dir header-conf file-name]
   (->> (load-md-files dir)
        (map parse-md-vec)
        (map gen-excel-rows)
        (reduce into [])
-       (gen-excel file-name)))
+       (gen-excel header-conf file-name)))
 
 (defn -main [& args]
   (let [[opts args banner]
         (cli args)
         dir (first args)
-        filename (second args)]
+        header-conf (second args)
+        filename (last args)]
     (if-not (and
-             (= (count args) 2)
+             (= (count args) 3)
              (.endsWith filename ".xlsx")) 
       (do
         (println "")
         (println "Usage: checklist <markdown files dir> <filename for generated excel (.xlsx)>"))
-      (checklist-to-excel dir filename))))
+      (checklist-to-excel dir header-conf filename))))
